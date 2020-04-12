d57b43894c7da14d9d5badbc31b2a0d45468b55b
Robert Ball <rob@coder73.com>
2020-04-12 14:59:21 +0100

    board config and blink code

--- Kit/Kit.ino
+++ Kit/Kit.ino
@@ -1,57 +1,37 @@
-#include <LiquidCrystal.h>
+/*
+  Blink
 
+  Turns an LED on for one second, then off for one second, repeatedly.
 
-int position = 0;
-int lastPosition = 0;
-int increment = 1;
-int pins[] = {3, 4, 5, 6};
-int sensorPin = A0;
-int sensorValue = 0;
-int outputValue = 0;
-int ticks = 0;
-const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
 
-void setup()
-{
-  lcd.begin(16, 2);
-  lcd.print("Hello");
-  Serial.begin(9600);
-  size_t n = sizeof(pins);
-  size_t i = 0;
-  for (i = 0; i < n; i++)
-  {
-    pinMode(pins[i], OUTPUT);
-  }
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  ticks++;
-  sensorValue = analogRead(sensorPin);
-  outputValue = map(sensorValue, 0, 1023, 0, 1000);
-  lcd.setCursor(0, 1);
-  lcd.print(ticks);
-
-  Serial.print("Position: ");
-  Serial.print(position);
-  Serial.print("\t Increment: ");
-  Serial.print(increment);
-  Serial.print("\t Last Position: ");
-  Serial.print(lastPosition);
-  Serial.print("\t Delay: ");
-  Serial.println(outputValue);
-
-  digitalWrite(pins[lastPosition], LOW);
-  digitalWrite(pins[position], HIGH);
-
-  lastPosition = position;
-  position = position + increment;
-
-  if (position == 0 || position == 3)
-  {
-    increment = -increment;
-  }
-
-  delay(outputValue);
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
