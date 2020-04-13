2d4748c0048186e25376e8b8b455173b79d24997
Robert Ball <rob@coder73.com>
2020-04-13 17:25:02 +0100

    Buzzer

--- Digital_Inputs/Digital_Inputs.ino
+++ Digital_Inputs/Digital_Inputs.ino
@@ -1,29 +1,49 @@
+#include "pitches.h";
 #define PIN3 3
 #define PIN7 7
+#define BUZZER 8
 #define PIN13 13
 
-int P3 = 0;
+int buttonState = 0;
 int P13 = 0;
+
+int melody[] = {
+    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int durations[] = {
+    4, 8, 8, 4, 4, 4, 4, 4};
+
 void setup()
 {
     Serial.begin(9600);
 
     pinMode(PIN3, INPUT);
     pinMode(PIN7, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
     pinMode(PIN13, OUTPUT);
 }
 
 void loop()
 {
-    P3 = digitalRead(PIN3);
+    buttonState = digitalRead(PIN3);
 
     digitalWrite(13, LOW);
 
-    Serial.println(P3);
+    Serial.println(buttonState);
 
-    if (P3 == 1)
+    if (buttonState == 1)
     {
         digitalWrite(PIN7, HIGH);
+
+        for (int note = 0; note < 8; note++)
+        {
+            int duration = 1000 / durations[note];
+            tone(BUZZER, melody[note], duration);
+            int pause = duration * 1.30;
+            delay(pause);
+            noTone(BUZZER);
+        }
     }
     else
     {
