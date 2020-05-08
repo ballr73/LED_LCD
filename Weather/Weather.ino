d18bececd17be582dafa44c3d62b9bedf359833c
Robert Ball <rob@coder73.com>
2020-05-08 17:01:51 +0100

    basic led coms

--- Weather/Weather.ino
+++ Weather/Weather.ino
@@ -1,16 +1,34 @@
 #include <LiquidCrystal.h>
+
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+const int v0 = 6;
+const int con = 100;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 // the setup function runs once when you press reset or power the board
 void setup()
 {
     Serial.begin(9600);
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
+    analogWrite(v0,con);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
     Serial.println("tick...");
+    
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis() / 1000);
+    
     digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
     delay(100);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
